7c58e0ca4dbcb6c34ffdba4da599009d6771bb3c
jim_wu <jim40513@gmail.com>
2016-05-04 00:32:15 +0800

    async.whilst_ver2

--- mobile01_topiclist.js
+++ mobile01_topiclist.js
@@ -15,11 +15,11 @@ var db = new mongodb.Db('FAKE', mongodbServer);
 
 // for (var page = 1; page <= 2; page++) {
 var page = 1;
-
+var pages,desc,href,dt,authur;
 async.whilst(
 
     function() {
-        return page < 2;
+        return page <= 3;
     },
 
     function(callback) {
@@ -34,39 +34,50 @@ async.whilst(
         request(options, function(error, response, body) {
             if (error) return console.log(error)
             $ = cheerio.load(body);
+            //console.log(body);
+            
             if (page == 1) {
-                //爬主題下的回文
-                $('tbody>tr').each(function(i, elem) {
-                    var desc = $(elem).find('.subject-text>a').text()
-                    var href = "http://www.mobile01.com/" + $(elem).find('.subject-text a').attr('href')
-                    var dt = $(elem).find('p').first().text()
-                    var authur = $(elem).find('p').last().text()
-
-
-                }); //each end
+                console.log('123');
+                // //爬主題下的回文
+                // $('tbody>tr').each(function(i, elem) {
+                //     var desc = $(elem).find('.subject-text>a').text()
+                //     var href = "http://www.mobile01.com/" + $(elem).find('.subject-text a').attr('href')
+                //     var dt = $(elem).find('p').first().text()
+                //     var authur = $(elem).find('p').last().text()
+                // }); //each end
                 // console.log("===============");
                 // console.log("主題：" + desc);
                 // console.log("主題url：" + href);
                 // console.log("發文時間：" + dt);
                 // console.log("發文作者：" + authur);
             }
+            if ((pages + 1) == page) {
+                page = 0;
+            } else {
+                console.log('321');
+                //爬主題下的回文
+                $('tbody>tr').each(function(i, elem) {
+                    var desc = $(elem).find('.subject-text>a').text()
+                    var href = "http://www.mobile01.com/" + $(elem).find('.subject-text a').attr('href')
+                    var dt = $(elem).find('p').first().text()
+                    var authur = $(elem).find('p').last().text()
+                }); //each end
+                console.log("===============");
+                console.log("主題：" + desc);
+                console.log("主題url：" + href);
+                console.log("發文時間：" + dt);
+                console.log("發文作者：" + authur);
+            }
 
+            setTimeout(function() {
+                callback(null, page);
+            }, 50);
         }); //request end
-        page++;
-        setTimeout(function() {
-            callback(null, page);
-        }, 1000);
-    },
-    function(err) {
-        if (err) {
-            console.log("err:" + err);
-            // done(err);
-        } else {
-            console.log("===============");
-            console.log("主題：" + desc);
-            console.log("主題url：" + href);
-            console.log("發文時間：" + dt);
-            console.log("發文作者：" + authur);
+    }, //function(callback) end
+    function(error) {
+        if (error) {
+            console.log("error:" + error);
+            // done(error);
         }
     }
     // } //for
