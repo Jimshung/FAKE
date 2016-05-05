91c302df7d6141a77c329080125f8bf460cb3281
jim_wu <jim40513@gmail.com>
2016-05-05 16:25:20 +0800

    發文作者－update

--- mobile01_topiclist.js
+++ mobile01_topiclist.js
@@ -12,17 +12,14 @@ var mongodbServer = new mongodb.Server('localhost', 27017, {
 });
 var db = new mongodb.Db('FAKE', mongodbServer);
 
-
-
 var page = 0;
 var pages, desc, href, dt, authur;
 async.whilst(
 
     function() {
-        console.log("頁數："+page);
         page++;
-        return page <= 20;
-
+        console.log("page_value：" + page);
+        return page <= 1;//控制要抓mobile01_topiclist幾頁
     },
 
     function(callback) {
@@ -33,25 +30,23 @@ async.whilst(
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36',
             }
         };
-        console.log(p_url);
+        console.log("now_page：" + p_url);
         request(options, function(error, response, body) {
             if (error) return callback(error)
             $ = cheerio.load(body);
-            //console.log(body);
-
-            console.log('321');
-            //爬主題下的回文
+            console.log('爬mobile01_topiclist的主題文');
+            //第一頁31個貼文，後續一頁30個貼文
             $('tbody>tr').each(function(i, elem) {
                 var desc = $(elem).find('.subject-text>a').text()
                 var href = "http://www.mobile01.com/" + $(elem).find('.subject-text a').attr('href')
                 var dt = $(elem).find('p').first().text()
-                var authur = $(elem).find('p').last().text()
+                var authur = $(elem).find('.authur a p').last().text()
                 console.log("===============");
                 console.log("主題：" + desc);
                 console.log("主題url：" + href);
                 console.log("發文時間：" + dt);
                 console.log("發文作者：" + authur);
-            }); 
+            });
 
             setTimeout(function() {
                 callback(null, page);
@@ -61,7 +56,7 @@ async.whilst(
     function(error) {
         if (error) {
             console.log("error:" + error);
-            // done(error);
+            
         }
     }
 ); //async.whilst end
