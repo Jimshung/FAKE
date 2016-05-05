49b222017d4eb2db0eeb5233417eb2e2946655b3
jim_wu <jim40513@gmail.com>
2016-05-05 22:02:02 +0800

    DB-version1

--- mobile01_topiclist.js
+++ mobile01_topiclist.js
@@ -13,13 +13,13 @@ var mongodbServer = new mongodb.Server('localhost', 27017, {
 var db = new mongodb.Db('FAKE', mongodbServer);
 
 var page = 0;
-var pages, desc, href, dt, authur;
+
 async.whilst(
 
     function() {
         page++;
         console.log("page_value：" + page);
-        return page <= 1;//控制要抓mobile01_topiclist幾頁
+        return page <= 1; //控制要抓mobile01_topiclist幾頁
     },
 
     function(callback) {
@@ -30,33 +30,65 @@ async.whilst(
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36',
             }
         };
-        console.log("now_page：" + p_url);
+        console.log("now_url：" + p_url);
+
         request(options, function(error, response, body) {
             if (error) return callback(error)
             $ = cheerio.load(body);
             console.log('爬mobile01_topiclist的主題文');
             //第一頁31個貼文，後續一頁30個貼文
+            var subject = [];
             $('tbody>tr').each(function(i, elem) {
-                var desc = $(elem).find('.subject-text>a').text()
-                var href = "http://www.mobile01.com/" + $(elem).find('.subject-text a').attr('href')
-                var dt = $(elem).find('p').first().text()
-                var authur = $(elem).find('.authur a p').last().text()
+                subject[i] = {
+                    desc: $(elem).find('.subject-text>a').text(),
+                    href: "http://www.mobile01.com/" + $(elem).find('.subject-text a').attr('href'),
+                    dt: $(elem).find('p').first().text(),
+                    authur: $(elem).find('.authur a p').last().text()
+                }
                 console.log("===============");
-                console.log("主題：" + desc);
-                console.log("主題url：" + href);
-                console.log("發文時間：" + dt);
-                console.log("發文作者：" + authur);
+                console.log("主題：" + subject[i].desc);
+                console.log("主題url：" + subject[i].href);
+                console.log("發文時間：" + subject[i].dt);
+                console.log("發文作者：" + subject[i].authur);
+
+                /* open db */
+                db.open(function() {
+                    /* Select 'contact' collection */
+                    db.collection('mobile01_post', function(err, collection) {
+                        /* Insert a data */
+                        for (var i = 0; i < subject.length; i++) {
+                            collection.insert({
+                                desc: subject.desc,
+                                href: subject.href,
+                                dt: subject.dt,
+                                authur: subject.authur
+                            }, function(err, data) {
+                                if (data) {
+                                    console.log('Successfully Insert');
+                                } else {
+                                    console.log('Failed to Insert');
+                                }
+                            });
+
+                        }
+
+                    });
+                });
+
             });
+
 
             setTimeout(function() {
                 callback(null, page);
             }, 2500);
         }); //request end
     }, //function(callback) end
+
+
     function(error) {
         if (error) {
             console.log("error:" + error);
-            
+
         }
     }
 ); //async.whilst end
