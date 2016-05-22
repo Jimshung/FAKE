9f9708f38144e1d891858577435d533cd759156f
jim_wu <jim40513@gmail.com>
2016-05-22 19:57:30 +0800

    ckip 1.文章篇幅太常,不會進行ckip 2.有些Reply_content是空值 (貼圖)，要在post_detail就先處理

--- ckip.js
+++ ckip.js
@@ -7,8 +7,6 @@ var async = require('async');
 var mongodb = require('mongodb');
 var Promise = require('es6-promise').Promise;
 var ckip = require('ckip-client');
-var sleep = require('sleep');
-
 
 var mongodbServer = new mongodb.Server('localhost', 27017, {
     auto_reconnect: true,
@@ -17,75 +15,72 @@ var mongodbServer = new mongodb.Server('localhost', 27017, {
 var db = new mongodb.Db('FAKE', mongodbServer);
 
 var open_db_function = function(table, func_in) {
-                            //console.log('db1',db);
-
     db.open(function() {
         db.collection(table, function(err, collection) {
             func_in(collection, err);
-
         });
     });
-
 };
 
 var ckip = require('ckip-client')('140.109.19.104', 1501, 'jim51114', 'abc6541');
+/*  待處理
+1.文章篇幅太常,不會進行ckip
+2.有些Reply_content是空值 (貼圖)，要在post_detail就先處理
+*/
+
 
 open_db_function(
     'post_detail',
     function(collection, err) {
         collection.find({}, { 'Reply_content': 1, _id: 0 }).toArray(function(err, data) {
             if (data) {
-              var i=0;
-              var for_loop = function(){
-                if(i < 3){
-                  console.log("i < 3");
-                  element = data[i];
-                  //i+1;
-                  console.log("element content", element.Reply_content);
-                  //sleep.sleep(2);
-                    ckip.request(element.Reply_content)
-                        .then(function(response) {
-                            console.log("run ckip 1")
-                            return Promise.all([ckip.getSentences(response), ckip.getTerms(response)]);
-                        })
-                        .then(function(results) {
-                            console.log("run ckip 2")
+                var i = 0;
+                // console.log("element："+element);
 
-                            console.log(results[0]); // [ ' 台新(N) 金控(N) 12月(N) 3日(N) 將(ADV) 召開(Vt) 股東(N) 臨時會(N) 進行(Vt) 董監(N) 改選(Nv) 。(PERIODCATEGORY)' ] 
-                            db.collection('post_detail_ckip', function(err, collection2) {
-                                //console.log("collection2",collection2);
-                                collection2.insert({"ckip_Result_0":results[0],"ckip_Result_1":results[1]}, function(err, data) {
-                                    if (data) {
-                                        console.log('Successfully Insert');
-                                    } else {
-                                        console.log('Failed to Insert');
-                                    }
+                var for_loop = function() {
+                    if (i < 300) {
+                        console.log("i < 300");
+                        element = data[i];
+                        console.log("Reply_content：", element.Reply_content);
+                        ckip.request(element.Reply_content)
+                            .then(function(response) {
+                                return Promise.all([ckip.getSentences(response), ckip.getTerms(response)]);
+                            })
+                            .then(function(results) {
+                                console.log("斷句：" + results[0]); // [ ' 台新(N) 金控(N) 12月(N) 3日(N) 將(ADV) 召開(Vt) 股東(N) 臨時會(N) 進行(Vt) 董監(N) 改選(Nv) 。(PERIODCATEGORY)' ] 
+                                // console.log("斷詞："+results[1]);
+                                db.collection('post_detail_ckip', function(err, collection2) {
+                                    collection2.insert({ "ckip_getSentences": results[0], "ckip_getTerms": results[1] }, function(err, data) {
+                                        if (data) {
+                                            console.log('Successfully Insert');
+                                        } else {
+                                            console.log('Failed to Insert');
+                                        }
+                                    });
                                 });
+                            })
+                            .catch(function(err) {
+                                console.log(err);
                             });
-                        })
-                        .catch(function(err) {
-                            console.log(err);
-                        });
-                      i = i+1;
-                      console.log("i",i);
+                        i = i + 1;
+                        console.log("第" + i + "篇評論");
+                    } else {
+                        console.log("i >= 300");
+                        clearInterval(for_loop);
                     }
-                    else{
-                      console.log("i >= 3");
-                      clearInterval(for_loop);
-                    }
-                  }
-               setInterval(  for_loop , 2000);
-               if(i>=3){
-                  console.log("out, i >= 3");
-                  clearInterval(for_loop);
-               }
+                }
+                setInterval(for_loop, 4000);
+                if (i >= 300) {
+                    console.log("out, i >= 300");
+                    clearInterval(for_loop);
+                }
             } else {
                 throw new Error(err);
             }
-
         })
     }
 );
+
 
 
 /*
@@ -147,56 +142,3 @@ open_db_function(
         })
     }
 );*/
-
-
-/*
-open_db_function(
-    'post_detail',
-    function(collection, err) {
-        collection.find({}, { 'Reply_content': 1, _id: 0 }).toArray(function(err, data) {
-            if (data) {
-                data.forEach(function(element) {
-
-                //element = data[0];
-
-                console.log("element content", element.Reply_content);
-
-                sleep.sleep(2)
-                });
-            } else {
-                throw new Error(err);
-            }
-
-        })
-    }
-);
-
-*/
-/*
-var ckip = require('ckip-client')('140.109.19.104', 1501, 'jim51114', 'abc6541');
-
-ckip.request('台新金控12月3日將召開股東臨時會進行董監改選。')
-    .then(function(response) {
-        return Promise.all([ckip.getSentences(response), ckip.getTerms(response)]);
-    })
-    .then(function(results) {
-        console.log(results[0]); // [ ' 台新(N) 金控(N) 12月(N) 3日(N) 將(ADV) 召開(Vt) 股東(N) 臨時會(N) 進行(Vt) 董監(N) 改選(Nv) 。(PERIODCATEGORY)' ] 
-        console.log(results[1]); // [ { term: '台新', tag: 'N' }, 
-        //   { term: '金控', tag: 'N' }, 
-        //   { term: '12月', tag: 'N' }, 
-        //   { term: '3日', tag: 'N' }, 
-        //   { term: '將', tag: 'ADV' }, 
-        //   { term: '召開', tag: 'Vt' }, 
-        //   { term: '股東', tag: 'N' }, 
-        //   { term: '臨時會', tag: 'N' }, 
-        //   { term: '進行', tag: 'Vt' }, 
-        //   { term: '董監', tag: 'N' }, 
-        //   { term: '改選', tag: 'Nv' }, 
-        //   { term: '。', tag: 'PERIODCATEGORY' } ] 
-    })
-    .catch(function(err) {
-        console.log(err);
-    });
-
-*/
-//斷詞服務採用TCP Socket連線傳輸資料，伺服器IP位址為 140.109.19.104 ，連接埠為 1501， 建議設定等候伺服器回應時間約一至三分鐘。資料詳細格式與使用注意事項敬請參閱斷詞服務網頁上的說明。
--- getpostdetail.js
+++ getpostdetail.js
@@ -9,70 +9,12 @@ var Promise = require('es6-promise').Promise;
 var Promise = require('promise');
 var axios = require('axios');
 
-
-
 var mongodbServer = new mongodb.Server('localhost', 27017, {
     auto_reconnect: true,
     poolSize: 10
 });
 var db = new mongodb.Db('FAKE', mongodbServer);
 
-
-var getAPageAllPost = function(element, page, to_continue) {
-    console.log(element);
-    if (page) {
-        var this_href = element.href + '&p=' + page;
-        console.log("log1:" + element.href + '&p=' + page);
-
-    } else {
-        //console.log('element',element);
-        var this_href = element.href;
-        console.log("log2:" + element.href);
-    }
-    return axios.post(this_href, {
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36',
-            },
-            timeout: 3000,
-        })
-        .then(function(response) {
-            $ = cheerio.load(response.data);
-            var last_page = $('.pagination').find('a').last().attr('href').replace(/.*p=/g, "");
-            //last_page為分頁最後值
-
-            
-            db.collection('post_detail', function(err, collection) {
-                $('.single-post').each(function(i, elem) {
-                    var singlepost = {
-                            Reply_user: $(elem).find('.fn').text(),
-                            Reply_time: $(elem).find('.date').text(),
-                            Reply_content: $(elem).find('.single-post-content').text().replace(/\\r\\n|\\r|\\n|\s/g, "").replace(/.*:+.+(恕刪)./g, "")
-                        }
-                        console.log("===============");
-                        console.log(singlepost);
-                        collection.insert(singlepost, function(err, data) {
-                        if (data) {
-                            console.log('Successfully Insert');
-                        } else {
-                            console.log('Failed to Insert');
-                        }
-                    }); // save to db
-
-
-                });
-            });
-            
-            console.log('last_page_in', last_page);
-            console.log('to_continue', to_continue);
-            if (to_continue && last_page) {
-                return Promise.resolve(last_page);
-            } else {
-                console.log('to stop');
-                return Promise.resolve(false);
-            }
-        })
-        .catch(function(err) { console.log("error:" + err); })
-};
 
 var open_db_function = function(table, func_in) {
 
@@ -82,7 +24,6 @@ var open_db_function = function(table, func_in) {
         });
     });
 };
-
 
 open_db_function(
     'mobile01_post',
@@ -118,3 +59,57 @@ open_db_function(
         })
     }
 );
+
+var getAPageAllPost = function(element, page, to_continue) {
+    console.log(element);
+    if (page) {
+        var this_href = element.href + '&p=' + page;
+        console.log("log1:" + element.href + '&p=' + page);
+
+    } else {
+        //console.log('element',element);
+        var this_href = element.href;
+        console.log("log2:" + element.href);
+    }
+    return axios.post(this_href, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36',
+            },
+            timeout: 3000,
+        })
+        .then(function(response) {
+            $ = cheerio.load(response.data);
+            var last_page = $('.pagination').find('a').last().attr('href').replace(/.*p=/g, "");
+            //last_page為分頁最後值
+
+
+            db.collection('post_detail', function(err, collection) {
+                $('.single-post').each(function(i, elem) {
+                    var singlepost = {
+                        Reply_user: $(elem).find('.fn').text(),
+                        Reply_time: $(elem).find('.date').text(),
+                        Reply_content: $(elem).find('.single-post-content').text().replace(/\\r\\n|\\r|\\n|\s/g, "").replace(/.*:+.+(恕刪)./g, "")
+                    }
+                    console.log("===============");
+                    console.log(singlepost);
+                    collection.insert(singlepost, function(err, data) {
+                        if (data) {
+                            console.log('Successfully Insert');
+                        } else {
+                            console.log('Failed to Insert');
+                        }
+                    }); // save to db
+                });
+            });
+
+            console.log('last_page_in', last_page);
+            console.log('to_continue', to_continue);
+            if (to_continue && last_page) {
+                return Promise.resolve(last_page);
+            } else {
+                console.log('to stop');
+                return Promise.resolve(false);
+            }
+        })
+        .catch(function(err) { console.log("error:" + err); })
+};
